6903d6b87d4e15fcdd51b6610c3c94e9128f3a2c
J34NDERSON <113480398+J34NDERSON@users.noreply.github.com>
2025-01-26 14:38:39 -0300

    Add CarrinhoProvider in quintadimensaoliteraria/src/index.js

--- quintadimensaoliteraria/src/index.js
+++ quintadimensaoliteraria/src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {CarrinhoProvider}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <CarrinhoProvider>
+     <App />
+    </CarrinhoProvider>
   </React.StrictMode>
 );
 
